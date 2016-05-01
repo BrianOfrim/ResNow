947c0d739e7b0cd9f5e396244e605b03ff070e57
Brian Ofrim <bofrim@ualberta.ca>
2016-04-30 20:40:36 -0400

    front end work

---
+++ src/app/landing/landing.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'landing',
+  templateUrl: 'app/landing/landing.html',
+  styleUrls: ['app/landing/landing.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Landing {
+
+  constructor() {}
+
+}
